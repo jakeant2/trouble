93fde645290e22c001eda05969d6baa4be4a90b3
jakeant2 <55849880+jakeant2@users.noreply.github.com>
2025-08-15 11:56:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #start the game
 from game_controller import GameController
+from state import GameState
+from game_logic import GameLogic
+from player import Human
+from agents import RuleBasedAI, RandomAI
+from board import Board
+
+
 if __name__ == '__main__':
-    GameController().run()
+    state = GameState()
+    board = Board(state)
+    logic = GameLogic(board,state)
+    red_player = Human("RED")
+    blue_player = RuleBasedAI("BLUE")
+    players = [red_player, blue_player]
+
+    game = GameController(state, logic, players)
+    game.run()
